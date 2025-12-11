a57ba50b80f5f579634a37910b0eeac98efb8d2e
theMyle <99082612+theMyle@users.noreply.github.com>
2025-12-11 15:10:57 +0800

    refactor: Update LessonPage layout and replace header with paragraph for clarity

--- src/app/assessment/page.tsx
+++ src/app/assessment/page.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import MenuItem from "../components/MenuItem";
 
 export default function LessonPage() {
     return (
         <div
-            className={"flex flex-col gap-5 items-center justify-center h-full"}
+            className={"flex flex-col gap-6 items-center justify-center my-6 mx-4"}
         >
-            <h1 className={"text-4xl"}>Assessment</h1>
+            <p>Assessment</p>
+            <MenuItem
+                title="Array List - Assessment"
+                path="/assessment/array-list"
+            />
 
-            <Link href={"/assessment/array-list"}>ArrayList</Link>
             {/* <Link href={"/assessment/stack"}>Stack</Link>
             <Link href={"/assessment/queue"}>Queue</Link>
             <Link href={"/assessment/test"}>Test</Link> */}
