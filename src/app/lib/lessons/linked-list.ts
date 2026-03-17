fd1e5793f483189d2e73627dc2eb4889e7cfd06b
theMyle <99082612+theMyle@users.noreply.github.com>
2026-03-17 19:41:05 +0800

    linked list lesson titles fix

--- src/app/lib/lessons/linked-list.ts
+++ src/app/lib/lessons/linked-list.ts
@@ -3,25 +3,25 @@ import { Category } from "./types";
 export const LINKED_LIST: Category = {
   title: "Linked List",
   description:
-    "Learn how linked lists store data through connected nodes. This module covers what linked lists are, their core properties, and the efficiency of common operations such as insertion, deletion, and traversal.",
+    "Learn how linked lists store data through connected nodes. This module covers what linked lists are, their core properties, and the efficiency of common operations such as insertion, deletion, and traversals.",
   lessons: [
     {
-      title: "lessons 4.0",
+      title: "lessons 4.0 - Introduction",
       href: "/lesson/linkedlist/introduction",
       completed: false,
     },
     {
-      title: "lessons 4.1",
+      title: "lessons 4.1 - What is a Linked List",
       href: "/lesson/linkedlist/what-is-a-linkedlist",
       completed: false,
     },
     {
-      title: "lessons 4.2",
+      title: "lessons 4.2 - Linked List Properties",
       href: "/lesson/linkedlist/linkedlist-properties",
       completed: false,
     },
     {
-      title: "lessons 4.3",
+      title: "lessons 4.3 - Linked List Operations",
       href: "/lesson/linkedlist/linkedlist-operations",
       completed: false,
     },
